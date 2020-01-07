71da371ad5feb470803dcd8bb31d9c5b20541fb5
ousc <750154432@qq.com>
2020-01-07 19:27:28 +0800

    修复情况一题多解

--- app.py
+++ app.py
@@ -37,18 +37,34 @@ while True:
         try:
             question = wd.find_element_by_class_name("question").text # 获得题目
             question = question.split("\n")[0] # 去除题目作者信息
+
+            choices = []
+            answer_items = wd.find_elements_by_class_name("answer-item") # 获取所有选项
+            for answer_item in answer_items:
+                choices.append(answer_item.text.split(" ")[1])
+            choices = [i.encode("GBK") for i in choices]
+            choices.sort()
+            choices = [i.decode("GBK") for i in choices]
+            for choice in choices:
+                question = question + "|" + choice
         except:
             continue
 
         if(answerMap.get(question) != None): # 如果找到答案，根据答案点击正确答案，继续答下一题
-            answer_items = wd.find_elements_by_class_name("answer-item") # 获取选项
             answer = re.sub('[\n]+', '', answerMap[str(question)])
             print("找到答案，本题答案为" + answerMap[str(question)])
 
+            have = False
+
             for answer_item in answer_items:
                 if(str(answer_item.text).find(answer)!= -1 or str(answer_item.text) == answer):
+                    have = True
                     answer_item.click()
                     break
+            if(not have): # 一题多解情况
+                answer_items[0].click()
+                answerMap[str(question)] = answer + "&" + str(answer_item.text)
+
 
         else: # 否则默认选第一个，把正确答案加入答案库
             answer_items = wd.find_elements_by_class_name("answer-item") # 获取选项
--- data2Json.py
+++ data2Json.py
@@ -7,9 +7,12 @@ answer_arr = []
 line = f.readline()
 while line:
     if(len(line)>2):
-        q = line.split("#")[0]
+        q_and_choices  = line.split("#")[0].split("|")
+        q = q_and_choices[0]
+        c = q_and_choices[1:]
+        c = '、'.join(str(i) for i in c)
         a = re.sub('[\n]+', '', line.split("#")[1])
-        data = {"question":q, "answer":a}
+        data = {"question":q, "answer":a, "choices":c}
         answer_arr.append(data)
     line = f.readline()
 f.close()
